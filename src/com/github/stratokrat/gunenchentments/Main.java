a497708aebfe17e553a0cc87c312b0226c7d9d81
Sven van Crombrugge <s.crombrugge@gmail.com>
2018-02-19 12:02:17 +0100

    Java Projekt erstellt.

---
+++ src/com/github/stratokrat/gunenchentments/Main.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.github.stratokrat.gunenchentments;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.*;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+/**
+ * @author Sven
+ *
+ */
+public class Main extends JavaPlugin implements Listener {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(this, this);
+	}
+	
+	public void onDisable() {
+		saveConfig();
+	}
+	
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onEnchant(PrepareItemEnchantEvent e) {
+		
+	}
+	// Ammunition
+	
+
+}
